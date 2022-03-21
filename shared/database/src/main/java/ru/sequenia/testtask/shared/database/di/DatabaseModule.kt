efeba4eb3995cc2e90a32a895de5a294f4e4b435
Aprekek <mhlshamburov@gmail.com>
2022-03-21 22:55:16 +0700

    implemented di

---
+++ shared/database/src/main/java/ru/sequenia/testtask/shared/database/di/DatabaseModule.kt
@@ -0,0 +1,24 @@
+package ru.sequenia.testtask.shared.database.di
+
+import androidx.room.Room
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import ru.sequenia.testtask.shared.database.KeyGenerator
+import ru.sequenia.testtask.shared.database.database.FilmsDataBase
+
+val databaseModule = module {
+
+	single {
+		Room.databaseBuilder(
+			androidContext(),
+			FilmsDataBase::class.java,
+			FilmsDataBase.NAME
+		).build()
+	}
+
+	factory { get<FilmsDataBase>().filmsDao() }
+	factory { get<FilmsDataBase>().genreDao() }
+	factory { get<FilmsDataBase>().genreFilmLinksDao() }
+
+	single { KeyGenerator(sharedPreferences = get()) }
+}
