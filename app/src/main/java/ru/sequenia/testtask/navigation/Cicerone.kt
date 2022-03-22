6073ebc674b7f1e0a5ec3e1281c8247358655fac
Aprekek <mhlshamburov@gmail.com>
2022-03-22 22:14:33 +0700

    created films general screen

--- app/src/main/java/ru/sequenia/testtask/navigation/Cicerone.kt
+++ app/src/main/java/ru/sequenia/testtask/navigation/Cicerone.kt
@@ -3,5 +3,5 @@ package ru.sequenia.testtask.navigation
 import com.github.terrakok.cicerone.Cicerone
 
 fun provideCicerone() = Cicerone.create().apply {
-	// TODO router.newRootScreen(rootScreen)
+	router.newRootScreen(provideRootScreen())
 }
---
+++ features/films/general/src/main/java/ru/sequenia/testtask/features/films/general/presentation/screen/FilmsGeneralScreen.kt
@@ -0,0 +1,6 @@
+package ru.sequenia.testtask.features.films.general.presentation.screen
+
+import com.github.terrakok.cicerone.androidx.FragmentScreen
+import ru.sequenia.testtask.features.films.general.presentation.ui.FilmsGeneralFragment
+
+fun getFilmsGeneralScreen() = FragmentScreen { FilmsGeneralFragment.getInstance() }
