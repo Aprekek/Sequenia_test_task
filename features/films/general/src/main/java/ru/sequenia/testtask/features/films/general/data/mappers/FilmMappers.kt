28257567e1c89fd89e42a41b626af1df972bb204
Aprekek <mhlshamburov@gmail.com>
2022-03-23 11:58:27 +0700

    added addition field 'name' to film

--- features/films/general/src/main/java/ru/sequenia/testtask/features/films/general/data/mappers/FilmMappers.kt
+++ features/films/general/src/main/java/ru/sequenia/testtask/features/films/general/data/mappers/FilmMappers.kt
@@ -3,4 +3,4 @@ package ru.sequenia.testtask.features.films.general.data.mappers
 import ru.sequenia.testtask.features.films.general.data.models.FilmRemoteModel
 import ru.sequenia.testtask.shared.database.dto.FilmDto
 
-fun FilmRemoteModel.toDatabaseEntity() = FilmDto(id, localizedName, year, rating, imageUrl, description)
+fun FilmRemoteModel.toDatabaseEntity() = FilmDto(id, localizedName, name, year, rating, imageUrl, description)
