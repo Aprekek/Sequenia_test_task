85599b3fd45e8104aa7301397f7628786709bc9d
Aprekek <mhlshamburov@gmail.com>
2022-03-23 12:38:08 +0700

    fixed filmDetailsModule dependency

--- app/src/main/java/ru/sequenia/testtask/App.kt
+++ app/src/main/java/ru/sequenia/testtask/App.kt
@@ -5,6 +5,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import ru.sequenia.testtask.di.globalNavigationModule
 import ru.sequenia.testtask.di.routersModule
+import ru.sequenia.testtask.feature.films.details.di.filmDetailsModule
 import ru.sequenia.testtask.features.films.general.di.filmsGeneralModule
 import ru.sequenia.testtask.network.di.networkModule
 import ru.sequenia.testtask.shared.database.di.databaseModule
