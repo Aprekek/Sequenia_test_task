23f8b27c138f8b0875dbe782e1a91653ce732b04
Aprekek <mhlshamburov@gmail.com>
2022-03-23 20:19:51 +0700

    improved db performance

--- shared/films/src/main/java/ru/sequenia/testtask/shared/films/data/dao/FilmsDao.kt
+++ shared/films/src/main/java/ru/sequenia/testtask/shared/films/data/dao/FilmsDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import ru.sequenia.testtask.shared.films.data.dto.FilmAnnotationDto
 import ru.sequenia.testtask.shared.films.data.dto.FilmDto
 import ru.sequenia.testtask.shared.films.data.dto.FilmWithGenresDto
@@ -15,12 +16,14 @@ interface FilmsDao {
 	@Insert(onConflict = OnConflictStrategy.REPLACE)
 	suspend fun insert(films: List<FilmDto>)
 
+	@Transaction
 	@Query("SELECT * FROM films_table WHERE filmId = :filmId")
 	suspend fun get(filmId: Long): FilmWithGenresDto
 
 	@Query("SELECT filmId, localizedName, imageUrl FROM films_table ")
 	suspend fun getAll(): List<FilmAnnotationDto>
 
+	@Transaction
 	@Query("SELECT genreId FROM genre_table WHERE genreId = :genreId")
 	suspend fun getAllWithFilter(genreId: Long): GenreWithFilmsDto
 }
