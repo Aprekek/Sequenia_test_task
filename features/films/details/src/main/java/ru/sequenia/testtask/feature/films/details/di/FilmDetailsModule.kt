e37c83217334b9211de28cc4c7c147ace00c48f8
Aprekek <mhlshamburov@gmail.com>
2022-03-23 10:39:59 +0700

    created presenter

--- features/films/details/src/main/java/ru/sequenia/testtask/feature/films/details/di/FilmDetailsModule.kt
+++ features/films/details/src/main/java/ru/sequenia/testtask/feature/films/details/di/FilmDetailsModule.kt
@@ -1,11 +1,13 @@
 package ru.sequenia.testtask.feature.films.details.di
 
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import ru.sequenia.testtask.feature.films.details.data.datasource.FilmsDataSource
 import ru.sequenia.testtask.feature.films.details.data.datasource.FilmsDataSourceImpl
 import ru.sequenia.testtask.feature.films.details.data.repository.FilmsRepositoryImpl
 import ru.sequenia.testtask.feature.films.details.domain.repository.FilmsRepository
 import ru.sequenia.testtask.feature.films.details.domain.usecases.GetFilmUseCase
+import ru.sequenia.testtask.feature.films.details.presentation.presenter.FilmDetailsPresenter
 
 val filmDetailsModule = module {
 
@@ -14,4 +16,11 @@ val filmDetailsModule = module {
 	factory<FilmsRepository> { FilmsRepositoryImpl(datasource = get()) }
 
 	factory { GetFilmUseCase(repository = get()) }
+
+	viewModel {
+		FilmDetailsPresenter(
+			router = get(),
+			getFilmUseCase = get()
+		)
+	}
 }
